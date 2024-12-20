a65351faa195dfbb90daed1a5a4bcf394744e002
Swapnil Katwe <swapnil.katwe111@gmail.com>
2024-12-20 12:30:42 +0100

    Assets and Header component Added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Header from './component/Header'
 
 function App() {
 
   return (
     <>
-      <h1>KBC</h1>
+    <Header />
     </>
   )
 }
---
+++ src/component/Header.jsx
@@ -0,0 +1,9 @@
+import quizLogo from "../assets/quiz-logo.png";
+export default function Header() {
+    return(
+        <header>
+            <img src={quizLogo} alt="Quiz Logo"/>
+            <h1>KBC Quiz</h1>
+        </header>
+    );
+}
