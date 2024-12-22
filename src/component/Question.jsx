1a9eeb64e067444bfaa8793e967717c819dd3a3b
Swapnil Katwe <swapnil.katwe111@gmail.com>
2024-12-22 23:08:13 +0100

    Solved issue of answer mismatch due to last minute answer and logic of 2 and 3 seconds. Question component updated with progressbar timer logic.

--- src/component/Question.jsx
+++ src/component/Question.jsx
@@ -4,49 +4,60 @@ import Answers from "./Answers";
 import QuestionTimer from "./QuestionTimer";
 import QUESTIONS from "../questions.js";
 
-export default function Question({
-  index,
-  onSelectAnswer,
-  onskipAnswer,
-}) {
-
+export default function Question({ index, onSelectAnswer, onskipAnswer }) {
   const [answer, setAnswer] = useState({
     selectedAnswer: "",
-    isCorrect: null
+    isCorrect: null,
   });
 
+  // TIMER LOGIC TO SOLVE ANSWER MISSMATCH ISSUE DUE TO LAST MOMENT ANSWER SELECTION
+  let timer = 10000;
+
+  if (answer.selectedAnswer) {
+    timer = 1000;
+  }
+
+  if (answer.isCorrect !== null) {
+    timer = 2000;
+  }
+
   function handleSelectedAnswer(answer) {
+    // TO SET THE ANSWER WITH HIGHLIGHT THE SELECTED ANSWER
     setAnswer({
       selectedAnswer: answer,
-      isCorrect: null
+      isCorrect: null,
     });
 
-    setTimeout(()=> {
+    setTimeout(() => {
+      // TO SET THE RIGHT OR WRONG ANSWER AFTER 1000 MILISECONDS
       setAnswer({
         selectedAnswer: answer,
-        isCorrect: QUESTIONS[index].answers[0] === answer
+        isCorrect: QUESTIONS[index].answers[0] === answer,
       });
 
-      setTimeout(()=> {
+      // TO MOVE TO NEXT QUESTION WITH SELECTED ANSWER AFTER 2000 MILISECONDS DEPENDING ON USERS ACTION
+      setTimeout(() => {
         onSelectAnswer(answer);
-      },2000);
-
+      }, 2000);
     }, 1000);
-
-
   }
 
   let answerState = "";
-  
-  if(answer.selectedAnswer && answer.isCorrect !== null) {
+
+  if (answer.selectedAnswer && answer.isCorrect !== null) {
     answerState = answer.isCorrect ? "correct" : "wrong";
-  } else if(answer.selectedAnswer) {
-    answerState = "answered";   
+  } else if (answer.selectedAnswer) {
+    answerState = "answered";
   }
 
   return (
     <div id="question">
-      <QuestionTimer timeout={10000} onTimeout={onskipAnswer} />
+      <QuestionTimer
+        key={timer}
+        timeout={timer}
+        onTimeout={answer.selectedAnswer !== "" ? onskipAnswer : null }
+        mode={answerState}
+      />
       <h2>{QUESTIONS[index].text}</h2>
       <Answers
         answerState={answerState}
--- src/component/QuestionTimer.jsx
+++ src/component/QuestionTimer.jsx
@@ -1,27 +1,37 @@
 /* eslint-disable react/prop-types */
 import { useState, useEffect } from "react";
 
-export default function QuestionTimer({ timeout, onTimeout }) {
+export default function QuestionTimer({ timeout, onTimeout, mode }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
     console.log("set Timeout");
     const timer = setTimeout(onTimeout, timeout);
 
-    return ()=> {
-        clearInterval(timer);
-    }
+    return () => {
+      clearInterval(timer);
+    };
   }, [timeout, onTimeout]);
 
   useEffect(() => {
     console.log("set Interval");
     // Potential case of infinite loop as state updation every loop. So needed a wrap of useEffect to avoid issue.
-    const interval = setInterval(() => setRemainingTime((prevTime) => prevTime - 100), 100);
+    const interval = setInterval(
+      () => setRemainingTime((prevTime) => prevTime - 100),
+      100
+    );
 
-    return ()=> {
-        clearInterval(interval);
+    return () => {
+      clearInterval(interval);
     };
   }, []);
 
-  return <progress id="question" value={remainingTime} max={timeout} />;
+  return (
+    <progress
+      id="question"
+      value={remainingTime}
+      max={timeout}
+      className={mode}
+    />
+  );
 }
